0a497adb067fb453f38674f65975e2a95587b793
karenvillarreal91 <karenvillarreal9150@gmail.com>
2020-08-10 09:28:28 -0300

    Punto 2 Parcial

--- dos.js
+++ dos.js
@@ -7,5 +7,78 @@ b)la marca del más caro de los líquidos
 c)la marca del más liviano de los sólidos
 */function mostrar()
 {
-  alert("dos");
+  var marca;
+  var precio;
+  var peso;
+  var tipo;
+  var respuesta = true;
+  var sumaPeso = 0;
+  //mascaro
+  var flagMasCaro = true;
+  var marcaMasCaro;
+  var precioMasCaro;
+  //masliviano
+  var flagMasLiviano = true;
+  var marcaMasLiviano;
+  var pesoMasLiviano;
+
+  while(respuesta)
+  {
+    do
+    {
+      marca = prompt("Ingrese la marca del producto.");
+    }while(!isNaN(marca));
+
+    do
+    {
+      precio = prompt("Ingrese el precio del producto.");
+      precio = parseInt(precio);
+    }while(isNaN(precio) || precio < 1);
+
+    do
+    {
+      peso = prompt("Ingrese el peso en kg.");
+      peso = parseInt(peso);
+    }while(isNaN(peso) || peso < 0);
+
+    do
+    {
+      tipo = prompt("Ingrese de que tipo es el producto. (sólido, líquido)");
+    }while(tipo != "sólido" && tipo != "líquido");
+
+    //Punto A
+    sumaPeso += peso;
+
+    //Punto B
+    if(flagMasCaro && tipo == "líquido")
+    {
+      precioMasCaro = precio;
+      marcaMasCaro = marca;
+      flagMasCaro = false;
+    }
+    if(precio > precioMasCaro && tipo == "líquido")
+    {
+      precioMasCaro = precio;
+      marcaMasCaro = marca;
+    }
+
+    //Punto C
+    if(flagMasLiviano && tipo == "sólido")
+    {
+      pesoMasLiviano = peso;
+      marcaMasLiviano = marca;
+      flagMasLiviano = false;
+    }
+    if(peso < pesoMasLiviano && tipo == "sólido")
+    {
+      pesoMasLiviano = peso;
+      marcaMasLiviano = marca;
+    }
+
+    respuesta = confirm("Pruducto ingresado correctamente. ¿Desea continuar?");
+  }
+
+  document.write("El peso total de la compra es "+sumaPeso+".",
+  "<br>La marca del más caro de los líquidos es "+marcaMasCaro+".",
+  "<br>La marca del más liviano de los sólidos es "+marcaMasLiviano+".");
 }
